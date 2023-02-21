4f4df0df0a275d672222849edc45f951d39e72fb
Ryan <galazkaryan@gmail.com>
2023-02-21 13:05:59 -0500

    added role

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -36,3 +36,9 @@ module "security-groups" {
   source                            = "../modules/security-groups"
   vpc_id                            = module.vpc.vpc_id
 }
+
+# create iam role 
+module "ecs-task-execution-role" {
+  source                            = "../modules/ecs-task-execution-role"
+  project_name                      = module.vpc.project_name
+}
